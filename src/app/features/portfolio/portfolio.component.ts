5f49a5d83c6471b1d1652e2c864bbf711ce1902f
GeniltonSilva <geniltonsilva4002@gmail.com>
2024-10-06 19:41:07 -0300

    feat: mobile finishin

--- src/app/features/portfolio/portfolio.component.ts
+++ src/app/features/portfolio/portfolio.component.ts
@@ -53,6 +53,13 @@ export class PortfolioComponent {
 
 
 
+  scrollToSection(event: Event, sectionId: string): void {
+    event.preventDefault();
+    const element = document.getElementById(sectionId);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 
 
   
